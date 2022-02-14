254749e3f22cb98a1fdc4c70729c1e564f6ad8dd
Madan Chaudhary <madan712@gmail.com>
2022-02-14 13:57:11 +0000

    initial commit

---
+++ src/main/java/com/javaxp/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,24 @@
+package com.javaxp.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class LambdaFunctionHandler
+		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("event: " + gson.toJson(event));
+		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hello " + event.getBody() + "!");
+	}
+}
---
+++ src/test/java/com/javaxp/lambda/demo/LambdaFunctionHandlerTest.java
@@ -0,0 +1,39 @@
+package com.javaxp.lambda.demo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class LambdaFunctionHandlerTest {
+
+	private static APIGatewayProxyRequestEvent event;
+
+	@BeforeClass
+	public static void createInput() throws IOException {
+		event = new APIGatewayProxyRequestEvent();
+		event.setBody("Madan");
+	}
+
+	private Context createContext() {
+		TestContext ctx = new TestContext();
+		ctx.setFunctionName("test_function");
+		return ctx;
+	}
+
+	@Test
+	public void testLambdaFunctionHandler() {
+		LambdaFunctionHandler handler = new LambdaFunctionHandler();
+		Context ctx = createContext();
+		APIGatewayProxyResponseEvent output = handler.handleRequest(event, ctx);
+		Assert.assertEquals("Hello " + event.getBody() + "!", output.getBody());
+	}
+}
