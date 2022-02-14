b9a36677884fd672f2a31e4661ebe5a610ee392e
Madan Chaudhary <madan712@gmail.com>
2022-02-14 20:48:38 +0000

    added hash in terraform file

--- main.tf
+++ main.tf
@@ -19,13 +19,17 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_lambda_function" "terraform_lambda_demo" {
-  filename      = "target/demo-1.0.0.jar"
-  function_name = "terraformLambdaDemo"
-  role          = "arn:aws:iam::695663959248:role/service-role/lambdaDemo-role-43z4u7dd"
-  handler       = "com.javaxp.lambda.demo.LambdaFunctionHandler::handleRequest"
-  runtime       = "java11"
+locals {
+  build_file = "target/demo-1.0.0.jar"
+}
 
+resource "aws_lambda_function" "terraform_lambda_demo" {
+  filename         = local.build_file
+  function_name    = "terraformLambdaDemo"
+  role             = "arn:aws:iam::695663959248:role/service-role/lambdaDemo-role-43z4u7dd"
+  handler          = "com.javaxp.lambda.demo.LambdaFunctionHandler::handleRequest"
+  runtime          = "java11"
+  source_code_hash = filebase64sha256(local.build_file)
   tags = {
     Name = "terraformLambdaDemo"
   }
