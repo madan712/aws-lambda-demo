7f96afea8507dbaa15dc5510f26073240f471908
Madan Chaudhary <madan712@gmail.com>
2022-02-18 21:39:01 +0000

    Test

--- main.tf
+++ main.tf
@@ -19,18 +19,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "target/demo-1.0.0.jar"
-  output_path = "target/demo.zip"
-}
-
 resource "aws_lambda_function" "aws_lambda_demo" {
-  filename         = data.archive_file.lambda_zip.output_path
+  filename         = "target/demo-1.0.3.jar"
   function_name    = "awsLambdaDemo"
   role             = "arn:aws:iam::695663959248:role/service-role/lambdaDemo-role-43z4u7dd"
   handler          = "com.javaxp.lambda.demo.LambdaFunctionHandler::handleRequest"
   runtime          = "java11"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
 
