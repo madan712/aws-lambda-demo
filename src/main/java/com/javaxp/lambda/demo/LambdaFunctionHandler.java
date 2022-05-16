9c15ed03708f2b28022cb9512c7666e38e56ea18
Madan Chaudhary <madan712@gmail.com>
2022-05-16 11:56:08 +0100

    test

--- src/main/java/com/javaxp/lambda/demo/LambdaFunctionHandler.java
+++ src/main/java/com/javaxp/lambda/demo/LambdaFunctionHandler.java
@@ -12,7 +12,7 @@ import com.google.gson.GsonBuilder;
 public class LambdaFunctionHandler
 		implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private static final String GREETING = "Welcome back %s!";
+	private static final String GREETING = "Welcome %s!";
 
 	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
